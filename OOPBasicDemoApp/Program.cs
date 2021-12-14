1c142011c4b788be4531d13347406679cee37a6c
Patrick Andrews M <Patrickandrews.M@Ltts.com>
2021-12-14 10:48:18 +0530

    Day 2 demo code added

---
+++ OOPBasicDemoApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using OOPBasicDemoBusinessLogic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPBasicDemoApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to EMS");
+            Console.WriteLine("Enter PS Number");
+            int inPSNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Employee Name");
+            string inEmpName = Console.ReadLine();
+            Console.WriteLine("Enter Employee Contact Number");
+            string inEmpCtNumber = Console.ReadLine();            
+            DateTime inEmpDoJ = System.DateTime.Now;
+            //Constructor
+
+            Employee newEmpJoinee = new Employee(inPSNumber, inEmpName, inEmpCtNumber, inEmpDoJ);
+            Console.WriteLine("New Employee Onboarded");
+            Console.WriteLine("His/Her PS NUmber is "+ newEmpJoinee.GetPSNumber());
+        }
+    }
+}
---
+++ OOPBasicDemoBusinessLogic/Employee.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPBasicDemoBusinessLogic
+{
+    public class Employee
+    {
+        //Attriubutes : Member Variables : Properties
+
+        int PaySlipNumber;
+        string EmployeeName;
+        string EmployeeContactNumber;
+        DateTime EmployeeDateOfJoining;
+        
+        //Special Method with same name of class
+        //You do not need a return type
+
+        //Custom Default Constructor : NOT PROVIDED : THEN .NET Will automatically provision one
+        public Employee()
+        { // Initialisation before you start 
+            Console.WriteLine("This is a constructor");
+        }
+
+        //Parameterized Constructor
+        public Employee(int psnumber,string name,string phonenumber,DateTime dateofjoining)
+        {
+            if (psnumber > 0)
+            {
+                PaySlipNumber = psnumber;
+                EmployeeName = name;
+                EmployeeContactNumber = phonenumber;
+                EmployeeDateOfJoining = dateofjoining;
+            }
+           
+        }
+
+        //Get Method
+        public int GetPSNumber()
+        {
+            return PaySlipNumber;
+        }
+
+    }
+}
